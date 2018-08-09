1905af9bf2f11ea207db8d2b9f4a5a90f0cfbd64
Shameek Ganguly <shameekg@stanford.edu>
2018-08-09 00:54:59 -0700

    Updated install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,36 +1,45 @@
+dirs -c &&
 mkdir -p stanford_project &&
 pushd stanford_project &&
 
 # install chai3d
-git clone git@github.com:manips-sai-org/chai3d.git &&
+if [ ! -d "chai3d" ]; then
+	git clone git@github.com:manips-sai-org/chai3d.git
+fi
 pushd chai3d &&
 mkdir -p build &&
 pushd build &&
-cmake .. -DCMAKE_BUILD_TYPE=Release &&
+cmake .. -DCMAKE_BUILD_TYPE=Release && make &&
 popd && popd &&
 
 # copy sai2-simulation
 popd &&
-mv sai2-simulation.zip stanford_project/ &&
+cp sai2-simulation.zip stanford_project/ &&
 pushd stanford_project &&
 
 # install sai2-simulation
-unzip sai2-simulation.zip &&
-pushd sai2-simulation &&
+if [ ! -d "sai2-simulation" ]; then
+	unzip sai2-simulation.zip 
+fi
+pushd sai2-simulation-master &&
 mkdir -p build &&
 pushd build &&
-cmake .. -DCMAKE_BUILD_TYPE=Release &&
+cmake .. -DCMAKE_BUILD_TYPE=Release && make &&
 popd && popd &&
 
 # install sai2-common
-git clone git@github.com:manips-sai-org/sai2-common.git &&
+if [ ! -d "sai2-common" ]; then
+	git clone git@github.com:manips-sai-org/sai2-common.git
+fi
 pushd sai2-common &&
 mkdir -p build &&
 pushd build &&
-cmake .. -DCMAKE_BUILD_TYPE=Release &&
+cmake .. -DCMAKE_BUILD_TYPE=Release && make &&
 popd && popd &&
 
 # install project code
-git clone git@github.com:shameekganguly/short_project.git &&
+if [ ! -d "short_project" ]; then
+	git clone git@github.com:manips-sai-org/short_project.git
+fi
 pushd short_project &&
 sh build.sh
