468bfda0906dba269a46ff265c270fd780529346
Shameek Ganguly <shameekg@stanford.edu>
2018-08-17 11:41:57 -0700

    Updated install script for installing tinyxml and rbdl

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@ pushd stanford_project &&
 
 # install chai3d
 if [ ! -d "chai3d" ]; then
-	git clone git@github.com:manips-sai-org/chai3d.git
+	git clone git@github.com:manips-sai-org/chai3d.git || exit 1
 fi
 pushd chai3d &&
 mkdir -p build &&
@@ -19,7 +19,7 @@ pushd stanford_project &&
 
 # install sai2-simulation
 if [ ! -d "sai2-simulation" ]; then
-	unzip sai2-simulation.zip 
+	unzip sai2-simulation.zip  || exit 1
 fi
 pushd sai2-simulation-master &&
 mkdir -p build &&
@@ -27,9 +27,37 @@ pushd build &&
 cmake .. -DCMAKE_BUILD_TYPE=Release && make &&
 popd && popd &&
 
+# install tinyxml
+if [ ! -d "tinyxml2" ]; then
+	git clone git@github.com:leethomason/tinyxml2.git || exit 1
+fi
+pushd tinyxml2 && 
+git checkout 4.0.1 &&
+mkdir -p build &&
+pushd build &&
+cmake .. -DCMAKE_BUILD_TYPE=Release && make &&
+echo "Please enter computer password: " &&
+sudo make install &&
+popd && popd &&
+
+# install rbdl
+# TODO: make SHA free paths
+if [ ! -d "rbdl-rbdl-0879ee8c548a" ]; then
+	curl https://bitbucket.org/rbdl/rbdl/get/default.zip -o rbdl.zip &&
+	unzip rbdl.zip || exit 1
+fi
+pushd rbdl-rbdl-0879ee8c548a &&
+mkdir -p build &&
+pushd build &&
+cmake .. -DCMAKE_BUILD_TYPE=Release -DRBDL_BUILD_ADDON_URDFREADER=ON -D && make &&
+echo "Please enter computer password: " &&
+sudo make install &&
+popd && popd &&
+
+
 # install sai2-common
 if [ ! -d "sai2-common" ]; then
-	git clone git@github.com:manips-sai-org/sai2-common.git
+	git clone git@github.com:manips-sai-org/sai2-common.git  || exit 1
 fi
 pushd sai2-common &&
 mkdir -p build &&
@@ -39,7 +67,7 @@ popd && popd &&
 
 # install project code
 if [ ! -d "short_project" ]; then
-	git clone git@github.com:shameekganguly/short_project.git
+	git clone git@github.com:shameekganguly/short_project.git || exit 1
 fi
 pushd short_project &&
 sh build.sh
